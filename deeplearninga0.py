21df3f76510f2a91e300e0bfbb32253acef2b281
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-09-24 23:13:53 +0300

    day2

--- deeplearninga0.py
+++ deeplearninga0.py
@@ -20,3 +20,14 @@ train_data = train_in_df.to_numpy()
 train_label = train_out_df.to_numpy().flatten()
 test_data = test_in_df.to_numpy()
 test_label = test_out_df.to_numpy().flatten()
+
+# PCA
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(train_data)
+
+# t-SNE
+tsne = TSNE(n_components=2, perplexity=30, n_iter=1500)
+tsne_result = tsne.fit_transform(train_data)
+
+# UMAP
+umap_result = umap.UMAP(n_neighbors=5, min_dist=0.3).fit_transform(train_data)
